5eb904b119c53ea58be1bbea4dca7a0faeead6c9
cluke009 <gramos1984@gmail.com>
2012-10-04 07:08:33 -0400

    clean up formatting

--- js/pages/commment.js
+++ js/pages/commment.js
@@ -1,21 +1,23 @@
-var drupalgap_page_comment_cid; // other's set this cid so this page knows which comment to load
+// Other's set this cid so this page knows which comment to load.
+var drupalgap_page_comment_cid;
 
-$('#drupalgap_page_comment').live('pageshow',function(){
-	try {
-		// load comment
-		options = {
-			"cid":drupalgap_page_comment_cid,
-			"error":function(jqXHR, textStatus, errorThrown) {
-				alert("drupalgap_page_comment - failed to load comment (" + drupalgap_page_comment_cid + ")");
-			},
-			"success":function(comment) {
-				$('#drupalgap_page_comment .content').html(drupalgap_services_comment_render(comment));
-			},
-		};
-		drupalgap_services_comment_retrieve.resource_call(options);
-	}
-	catch (error) {
-		console.log("drupalgap_page_comment");
-		console.log(error);
-	}
+$('#drupalgap_page_comment').live('pageshow', function () {
+  try {
+    // Load comment.
+    options = {
+      "cid": drupalgap_page_comment_cid,
+      "error": function (jqXHR, textStatus, errorThrown) {
+        alert("drupalgap_page_comment - failed to load comment (" + drupalgap_page_comment_cid + ")");
+      },
+      "success": function (comment) {
+        $('#drupalgap_page_comment .content')
+          .html(drupalgap_services_comment_render(comment));
+      },
+    };
+    drupalgap_services_comment_retrieve.resource_call(options);
+  }
+  catch (error) {
+    console.log("drupalgap_page_comment");
+    console.log(error);
+  }
 });
--- js/services/content.js
+++ js/services/content.js
@@ -5,24 +5,33 @@
  *  A JSON object containing a list of {node_types} table data.
  *
  */
-function drupalgap_services_content_types_list () {
-	// If we have a copy of the content types list use it, otherwise make the
-	// service call.
-	if (!drupalgap_content_types_list) {
-		resource_path = "drupalgap_content/content_types_list.json";
-		drupalgap_content_types_list = drupalgap_services.resource_call({"resource_path":resource_path});
-	}
-	return drupalgap_content_types_list;
+function drupalgap_services_content_types_list() {
+  // If we have a copy of the content types list use it, otherwise make the
+  // service call.
+  if (!drupalgap_content_types_list) {
+    resource_path = "drupalgap_content/content_types_list.json";
+    drupalgap_content_types_list = drupalgap_services.resource_call({
+      "resource_path": resource_path
+    });
+  }
+
+  return drupalgap_content_types_list;
 }
 
-function drupalgap_services_content_type_load (type) {
-	if (!type) { return null; }
-	content_type = null;
-	content_types = drupalgap_services_content_types_list();
-	$.each(content_types,function(index,value){
-		if (value.type == type) { content_type = value; return; }
-	});
-	return content_type;
+function drupalgap_services_content_type_load(type) {
+  if (!type) {
+    return null;
+  }
+  content_type = null;
+  content_types = drupalgap_services_content_types_list();
+  $.each(content_types, function (index, value) {
+    if (value.type == type) {
+      content_type = value;
+      return;
+    }
+  });
+
+  return content_type;
 }
 
 /**
@@ -31,12 +40,15 @@ function drupalgap_services_content_type_load (type) {
  * @return
  *  A JSON object containing a list of Drupal Content Types and their associative People->Permissions for the current user.
  */
-function drupalgap_services_content_types_user_permissions () {
-	// If we have a copy of the content type's user permissions use it,
-	// otherwise make the service call.
-	if (!drupalgap_content_types_user_permissions) {
-		resource_path = "drupalgap_content/content_types_user_permissions.json";
-		drupalgap_content_types_user_permissions = drupalgap_services.resource_call({"resource_path":resource_path});
-	}
-	return drupalgap_content_types_user_permissions;
+function drupalgap_services_content_types_user_permissions() {
+  // If we have a copy of the content type's user permissions use it,
+  // otherwise make the service call.
+  if (!drupalgap_content_types_user_permissions) {
+    resource_path = "drupalgap_content/content_types_user_permissions.json";
+    drupalgap_content_types_user_permissions = drupalgap_services.resource_call({
+      "resource_path": resource_path
+    });
+  }
+
+  return drupalgap_content_types_user_permissions;
 }
