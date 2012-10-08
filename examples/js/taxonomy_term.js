7db7dd4fd20171d8e3c53ecbafae1e67e534b993
cluke009 <gramos1984@gmail.com>
2012-10-08 01:29:54 -0400

    Add taxonomy_term service support.

---
+++ examples/js/taxonomy_term.js
@@ -0,0 +1,199 @@
+/**
+ * Tab swapping.
+ */
+$('a').live('click', function () {
+  var target = $(this.rel);
+  $('.content').not(target).hide();
+  target.toggle();
+
+  $('a.active').removeClass('active');
+  $(this).addClass('active');
+  $('body > div.content_active').removeClass('content_active');
+  $(this.rel).addClass('content_active');
+});
+
+/**
+ * Handles the submission of the taxonomy_term retrieve form.
+ */
+$('#submit_taxonomy_term_retrieve').live('click', function () {
+  try {
+    // Make call to the bundled user login service resource.
+    options = {
+      "tid": $('#input_taxonomy_term_retrieve_tid').val(),
+      "error": function (jqXHR, textStatus, errorThrown) {
+        if (errorThrown) {
+          console.error(errorThrown);
+        }
+        else {
+          console.log(textStatus);
+        }
+      },
+      "success": function () {
+        // Success...
+      }
+    };
+    // Make service call.
+    drupal_services_taxonomy_term_retrieve.resource_call(options);
+  }
+  catch (error) {
+    console.log("errorThrown: #submit_taxonomy_term_retrieve - " + error);
+  }
+  return false;
+});
+
+/**
+ * Handles the submission of the taxonomy_term create form.
+ */
+$('#submit_taxonomy_term_create').live('click', function () {
+  try {
+    // Build service call options.
+    options = {
+      "vid": $('#input_taxonomy_term_create_vid').val(),
+      "name": $('#input_taxonomy_term_create_name').val(),
+      "description": $('#input_taxonomy_term_create_description').val(),
+      "weight": $('#input_taxonomy_term_create_weight').val(),
+      "parent": $('#input_taxonomy_term_create_parent').val(),
+
+      "error": function (jqXHR, textStatus, errorThrown) {
+        if (errorThrown) {
+          console.log(errorThrown);
+        }
+        else {
+          console.log(textStatus);
+        }
+      },
+
+      "success": function (data) {
+        // Success...
+      },
+    };
+    // Make the service call.
+    drupal_services_taxonomy_term_create.resource_call(options);
+  }
+  catch (error) {
+    console.log("errorThrown: #submit_taxonomy_term_create - " + error);
+  }
+  return false;
+});
+
+/**
+ * Handles the submission of the taxonomy_term update form.
+ */
+$('#submit_taxonomy_term_update').live('click', function () {
+  try {
+    // Build service call options.
+    options = {
+      "vid": $('#input_taxonomy_term_update_vid').val(),
+      "tid": $('#input_taxonomy_term_update_tid').val(),
+      "name": $('#input_taxonomy_term_update_name').val(),
+      "description": $('#input_taxonomy_term_update_description').val(),
+      "weight": $('#input_taxonomy_term_update_weight').val(),
+      "parent": $('#input_taxonomy_term_update_parent').val(),
+
+      "error": function (jqXHR, textStatus, errorThrown) {
+        if (errorThrown) {
+          console.log(errorThrown);
+        }
+        else {
+          console.log(textStatus);
+        }
+      },
+
+      "success": function (data) {
+        // Success...
+      },
+    };
+    // Make the service call.
+    drupal_services_taxonomy_term_update.resource_call(options);
+  }
+  catch (error) {
+    console.log("errorThrown: #submit_taxonomy_term_update - " + error);
+  }
+  return false;
+});
+
+/**
+ * Handles the submission of the taxonomy_term delete form.
+ */
+$('#submit_taxonomy_term_delete').live('click', function () {
+  try {
+    // Build service call options.
+    options = {
+      "tid": $('#input_taxonomy_term_delete_tid').val(),
+      "error": function (jqXHR, textStatus, errorThrown) {
+        if (errorThrown) {
+          console.log(errorThrown);
+        }
+        else {
+          console.log(textStatus);
+        }
+      },
+      "success": function (taxonomy_term) {
+        // Success...
+      },
+    };
+    // Make the service call.
+    drupal_services_taxonomy_term_delete.resource_call(options);
+  }
+  catch (error) {
+    console.log("errorThrown: #submit_taxonomy_term_delete - " + error);
+  }
+  return false;
+});
+
+/**
+ * Handles the submission of the taxonomy_term index form.
+ */
+$('#submit_taxonomy_term_index').live('click', function () {
+  try {
+    // Build service call options.
+    options = {
+      "error": function (jqXHR, textStatus, errorThrown) {
+        if (errorThrown) {
+          console.log(errorThrown);
+        }
+        else {
+          console.log(textStatus);
+        }
+      },
+      "success": function (taxonomy_term) {
+        // Success...
+      },
+    };
+    // Load taxonomy_term via services call.
+    drupal_services_taxonomy_term_index.resource_call(options);
+  }
+  catch (error) {
+    console.log("errorThrown: #submit_taxonomy_term_index - " + error);
+  }
+  return false;
+});
+
+/**
+ * Handles the submission of the taxonomy_term select_nodes form.
+ */
+$('#submit_taxonomy_term_select_nodes').live('click', function () {
+  try {
+    // Build service call options.
+    options = {
+      "tid": $('#input_taxonomy_term_select_nodes_tid').val(),
+      "error": function (jqXHR, textStatus, errorThrown) {
+        if (errorThrown) {
+          console.log(errorThrown);
+        }
+        else {
+          console.log(textStatus);
+        }
+      },
+      "success": function (taxonomy_term) {
+        // Success...
+      },
+    };
+    // Make the service call.
+    drupal_services_taxonomy_term_select_nodes.resource_call(options);
+  }
+  catch (error) {
+    console.log("errorThrown: #submit_taxonomy_term_select_nodes - " + error);
+  }
+  return false;
+});
---
+++ js/services/taxonomy_term.js
@@ -0,0 +1,362 @@
+/**
+ * @file
+ * Controls interactions with the services taxonomy_term group.
+ *
+ * @todo Create validation for all passed in values?
+ * @todo Add Drupal version checking.
+ */
+
+// Global variables used to hold the latest system resource call results.
+var drupalgap_services_taxonomy_term_update_result;
+var drupalgap_services_taxonomy_term_delete_result;
+
+/**
+ * Return the results for a specified term id or FALSE if term id does not exist.
+ * @type {Object}
+ */
+var drupal_services_taxonomy_term_retrieve = {
+  "resource_path": function (options) {
+    if ($.isNumeric(options)) {
+      return "taxonomy_term/" + encodeURIComponent(options) + ".json";
+    }
+    else {
+      console.log("Error: services/taxonomy_term.js 'options.nid' is not a number.");
+    }
+  },
+  "resource_type": "get",
+
+  "resource_call": function (caller_options) {
+    try {
+      // Build the options for the service call.
+      options = {
+        "resource_path": this.resource_path(caller_options.tid),
+        "type": this.resource_type,
+        "async": true,
+        "success": this.success,
+        "error": this.error
+      };
+
+      // Attach error/success hooks if provided.
+      if (caller_options.error) {
+        options.hook_error = caller_options.error;
+      }
+      if (caller_options.success) {
+        options.hook_success = caller_options.success;
+      }
+
+      // Retrieve the taxonomy_term.
+      drupal_services.resource_call(options);
+    }
+    catch (error) {
+      console.log("drupal_services_taxonomy_term_retrieve");
+      console.log(error);
+    }
+  },
+
+  "error": function (jqXHR, textStatus, errorThrown) {
+    if (errorThrown) {
+      console.log(errorThrown);
+    }
+    else {
+      console.log(textStatus);
+    }
+  },
+
+  "success": function (data) {},
+
+  /**
+   * Removes a taxonomy_term from local storage.
+   *
+   * options.nid
+   *    The taxonomy_term id of the taxonomy_term to remove.
+   */
+  "local_storage_remove": function (options) {
+    type = this.resource_type;
+    resource_path = this.resource_path(options);
+    key = drupal_services_default_local_storage_key(type, resource_path);
+    window.localStorage.removeItem(key);
+    console.log("Removed from local storage (" + key + ")");
+  },
+};
+
+/**
+ * Creates a new taxonomy_term based on submitted values.
+ * Returns 1 if successful.
+ * @type {Object}
+ */
+var drupal_services_taxonomy_term_create = {
+  "resource_path": "taxonomy_term.json",
+  "resource_type": "post",
+
+  "resource_call": function (caller_options) {
+    try {
+      // Build service call data string.
+      data = "vid=" + encodeURIComponent(caller_options.vid);
+      data += "&name=" + encodeURIComponent(caller_options.name);
+      data += "&description=" + encodeURIComponent(caller_options.description);
+      data += "&weight=" + encodeURIComponent(caller_options.weight);
+      data += "&parent=" + encodeURIComponent(caller_options.parent);
+
+      // Build options for service call.
+      options = {
+        "resource_path": this.resource_path,
+        "type": this.resource_type,
+        "data": data,
+        "async": true,
+        "success": this.success,
+        "error": this.error
+      };
+
+      // Attach error/success hooks if provided.
+      if (caller_options.error) {
+        options.hook_error = caller_options.error;
+      }
+      if (caller_options.success) {
+        options.hook_success = caller_options.success;
+      }
+
+      // Make the service call to the taxonomy_term create resource.
+      drupal_services.resource_call(options);
+    }
+    catch (error) {
+      console.log("Error: services/taxonomy_term.js");
+      console.log("Object: drupal_services_taxonomy_term_create - " + error);
+    }
+  },
+  "error": function (jqXHR, textStatus, errorThrown) {
+    if (errorThrown) {
+      console.log(errorThrown);
+    }
+    else {
+      console.log(textStatus);
+    }
+  },
+
+  "success": function (data) {},
+};
+
+/**
+ * Updates a specified taxonomy_term based on submitted values.
+ * @type {Object}
+ */
+var drupal_services_taxonomy_term_update = {
+  "resource_path": function (options) {
+    if ($.isNumeric(options)) {
+      return "taxonomy_term/" + encodeURIComponent(options) + ".json";
+    }
+    else {
+      console.log("Error: services/taxonomy_term.js 'options.nid' is not a number.");
+    }
+  },
+  "resource_type": "put",
+
+  "resource_call": function (caller_options) {
+    try {
+      // Build service call data string.
+      data = "vid=" + encodeURIComponent(caller_options.vid);
+      data += "&name=" + encodeURIComponent(caller_options.name);
+      data += "&description=" + encodeURIComponent(caller_options.description);
+      data += "&weight=" + encodeURIComponent(caller_options.weight);
+      data += "&parent=" + encodeURIComponent(caller_options.parent);
+
+      // Build options for service call.
+      options = {
+        "resource_path": this.resource_path(caller_options.tid),
+        "type": this.resource_type,
+        "data": data,
+        "async": true,
+        "success": this.success,
+        "error": this.error
+      };
+
+      // Attach error/success hooks if provided.
+      if (caller_options.error) {
+        options.hook_error = caller_options.error;
+      }
+      if (caller_options.success) {
+        options.hook_success = caller_options.success;
+      }
+
+      // Make the service call.
+      drupal_services.resource_call(options);
+    }
+    catch (error) {
+      console.log("Error: services/taxonomy_term.js");
+      console.log("Object: drupal_services_taxonomy_term_update - " + error);
+    }
+  },
+
+  "error": function (jqXHR, textStatus, errorThrown) {
+    if (errorThrown) {
+      console.log(errorThrown);
+    }
+    else {
+      console.log(textStatus);
+    }
+  },
+
+  "success": function (data) {},
+};
+
+/**
+ * Deletes the specified taxonomy_term. Returns true if delete was successful.
+ * @type {Object}
+ */
+var drupal_services_taxonomy_term_delete = {
+  "resource_path": function (options) {
+    if ($.isNumeric(options)) {
+      return "taxonomy_term/" + encodeURIComponent(options) + ".json";
+    }
+    else {
+      console.log("Error: services/taxonomy_term.js 'options.nid' is not a number.");
+    }
+  },
+  "resource_type": "delete",
+
+  "resource_call": function (caller_options) {
+    try {
+      // Build the options to the service call.
+      options = {
+        "resource_path": this.resource_path(caller_options.tid),
+        "type": this.resource_type,
+        "async": true,
+        "success": this.success,
+        "error": this.error
+      };
+
+      // Attach error/success hooks if provided.
+      if (caller_options.error) {
+        options.hook_error = caller_options.error;
+      }
+      if (caller_options.success) {
+        options.hook_success = caller_options.success;
+      }
+
+      // Make the service call.
+      drupal_services.resource_call(options);
+    }
+    catch (error) {
+      console.log("Error: services/taxonomy_term.js");
+      console.log("Object: drupal_services_taxonomy_term_delete - " + error);
+    }
+  },
+
+  "error": function (jqXHR, textStatus, errorThrown) {
+    if (errorThrown) {
+      console.log(errorThrown);
+    }
+    else {
+      console.log(textStatus);
+    }
+  },
+
+  "success": function (data) {},
+};
+
+/**
+ * Return an array of optionally paged tids baed on a set of criteria.
+ *
+ * An example request might look like
+ *
+ * http://domain/endpoint/taxonomy_term?fields=tid,name&parameters[tid]=7&parameters[vid]=1
+ *
+ * This would return an array of objects with only tid and name defined, where
+ * tid = 7 and vid = 1.
+ *
+ * @type {Object}
+ */
+var drupal_services_taxonomy_term_index = {
+  "resource_path": "taxonomy_term.json",
+  "resource_type": "get",
+
+  "resource_call": function (caller_options) {
+    try {
+      // Build the service resource call options.
+      options = {
+        "resource_path": this.resource_path,
+        "type": this.resource_type,
+        "async": true,
+        "error": this.error,
+        "success": this.success,
+      };
+
+      // Attach error/success hooks if provided.
+      if (caller_options.error) {
+        options.hook_error = caller_options.error;
+      }
+      if (caller_options.success) {
+        options.hook_success = caller_options.success;
+      }
+
+      // Make the service call.
+      drupal_services.resource_call(options);
+    }
+    catch (error) {
+      console.log("Error: services/taxonomy_term.js");
+      console.log("Object: drupal_services_taxonomy_term_index - " + error);
+    }
+  },
+
+  "error": function (jqXHR, textStatus, errorThrown) {
+    if (errorThrown) {
+      console.log(errorThrown);
+    }
+    else {
+      console.log(textStatus);
+    }
+  },
+
+  "success": function (data) {},
+};
+
+/**
+ * Returns all nodes with provided taxonomy id.
+ * @type {Object}
+ */
+var drupal_services_taxonomy_term_select_nodes = {
+  "resource_path": "taxonomy_term/selectNodes.json",
+  "resource_type": "post",
+
+  "resource_call": function (caller_options) {
+    try {
+      // Build service call data string.
+      data = "tid=" + encodeURIComponent(caller_options.tid);
+
+      // Build the options to the service call.
+      options = {
+        "data": data,
+        "resource_path": this.resource_path,
+        "type": this.resource_type,
+        "async": true,
+        "success": this.success,
+        "error": this.error
+      };
+
+      // Attach error/success hooks if provided.resource_call
+      if (caller_options.error) {
+        options.hook_error = caller_options.error;
+      }
+      if (caller_options.success) {
+        options.hook_success = caller_options.success;
+      }
+      console.log('test')
+      // Make the service call.
+      drupal_services.resource_call(options);
+    }
+    catch (error) {
+      console.log("Error: services/taxonomy_term.js");
+      console.log("Object: drupal_services_taxonomy_term_select_nodes - " + error);
+    }
+  },
+
+  "error": function (jqXHR, textStatus, errorThrown) {
+    if (errorThrown) {
+      console.log(errorThrown);
+    }
+    else {
+      console.log(textStatus);
+    }
+  },
+
+  "success": function (data) {},
+};
