55122ee1232a9cd0b48111c8c4c5f50ade2130f7
cluke009 <gramos1984@gmail.com>
2012-10-07 21:43:22 -0400

    Update error handling.

--- js/services/node.js
+++ js/services/node.js
@@ -115,8 +115,8 @@ var drupal_services_node_create = {
       drupal_services.resource_call(options);
     }
     catch (error) {
-      console.log("drupal_services_node_create");
-      console.log(error);
+      console.log("Error: services/node.js");
+      console.log("Object: drupal_services_node_create - " + error);
     }
   },
   "error": function (jqXHR, textStatus, errorThrown) {
