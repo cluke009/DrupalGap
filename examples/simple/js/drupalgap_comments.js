49bb3dabf568ecb68d9bd597543f8b92efdf4336
cluke009 <gramos1984@gmail.com>
2012-10-12 19:17:37 -0400

    Remove docs and generator.

---
+++ examples/simple/js/drupalgap_comments.js
@@ -0,0 +1,48 @@
+/*global services*/
+services.viewsDataSource.drupalgapComments = function (options) {
+  try {
+    // Build the options for the service call.
+    options = {
+      type: 'get',
+      endPoint: '',
+      url: 'drupalgap/views_datasource/drupalgap_comments',
+      async: true,
+      success: this.hookSuccess,
+      error: this.hookError
+    };
+
+    // Make the service call.
+    services.resource(options);
+  }
+  catch (error) {
+    console.log('Error: drupalgap_comments.js');
+    console.log('Object: services.viewsDataSource.drupalgapComments - ' + error);
+  }
+};
+services.viewsDataSource.drupalgap_content = function (options) {
+  try {
+    // Build the options for the service call.
+    options = {
+      endPoint: '',
+      type: 'get',
+      url: 'drupalgap/views_datasource/drupalgap_content',
+      async: true,
+      success: this.hookSuccess,
+      error: this.hookError
+    };
+
+    // Make the service call.
+    services.resource(options);
+  }
+  catch (error) {
+    console.log('Error: drupalgap_content');
+    console.log('Object: services.viewsDataSource.drupalgap_content - ' + error);
+  }
+};
+
+$('#submit_drupalgap_comments').live('click', function () {
+  // Build service call options.
+
+  // Make service call.
+  services.viewsDataSource.drupalgap_content();
+});
